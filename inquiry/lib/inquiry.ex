fb825f2774ab1fe4ebf57e7c1e48becf10333f67
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-05-13 23:59:02 -0500

    setting basic functions

--- inquiry/lib/inquiry.ex
+++ inquiry/lib/inquiry.ex
@@ -1,6 +1,12 @@
 defmodule Inquiry do
   @moduledoc """
-  Documentation for Inquiry.
+  Inquiry allows you to extract data from a nested data structura
+  like the ones you get from parsing a JSON.
+
+  I allows you to use strings as: key_in_map_one.key2.0 to get data
+  from the nested structures.
+
+  The nested structures support by now Lists and Maps
   """
 
   @doc """
@@ -12,7 +18,12 @@ defmodule Inquiry do
       :world
 
   """
-  def hello do
-    :world
+  def inquiry(data, query) do
+    decomposed_query = String.split(query, ".")
+    _inquiry(data, decomposed_query)
+  end
+
+  def _inquiry(data, query) do
+    data
   end
 end
