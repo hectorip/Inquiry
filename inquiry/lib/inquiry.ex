b55a45f75617b9f86329ff23346b6b65ccfb851b
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-05-13 23:37:58 -0500

    starting project

---
+++ inquiry/lib/inquiry.ex
@@ -0,0 +1,18 @@
+defmodule Inquiry do
+  @moduledoc """
+  Documentation for Inquiry.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Inquiry.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ inquiry/test/inquiry_test.exs
@@ -0,0 +1,8 @@
+defmodule InquiryTest do
+  use ExUnit.Case
+  doctest Inquiry
+
+  test "greets the world" do
+    assert Inquiry.hello() == :world
+  end
+end
