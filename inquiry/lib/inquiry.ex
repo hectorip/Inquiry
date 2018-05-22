fcf11ff7ff9db1715092312e3a5bdd258abd2c4a
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2018-05-22 00:00:11 -0500

    docs fixes

--- inquiry/lib/inquiry.ex
+++ inquiry/lib/inquiry.ex
@@ -3,11 +3,11 @@ defmodule Inquiry do
   Inquiry allows you to extract data from a nested data structure
   like the ones you get from parsing a JSON.
 
-  It allows you to use strings as: key_in_map_one.key2.0 to get data
+  It allows you to use queries as: key_in_map_one.key2.0 to get data
   from the nested structures.
 
   The nested structures supported by now are Lists and Maps with string
-  keys, we're working in a new version supporting atoms as keys and Keywords.
+  keys, I'm working in a new version supporting atoms as keys and Keywords.
   """
 
   @doc """
@@ -34,7 +34,7 @@ defmodule Inquiry do
     {index, _} = Integer.parse(current_query)
     _inquiry(Enum.at(data, index), rest)
   end
+  # def _inquiry(data, [current_query|rest]) when is_list(data) and current_query
   def _inquiry(data, [current_query|rest]), do: data |> Map.get(current_query) |> _inquiry(rest) # lacks non-string keys querying
-
   # TODO: add atoms as keys and keyword lists
 end
